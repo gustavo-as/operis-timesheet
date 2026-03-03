2627172d42bf72faea490150a05ab908adc16204
AS Gustavo <gustavo.simoes@hotmail.com.br>
2026-03-03 19:43:06 +0100

    initial project setup

---
+++ src/main/java/com/operis/timesheet/OperisTimesheetApplication.java
@@ -0,0 +1,13 @@
+package com.operis.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperisTimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperisTimesheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/operis/timesheet/OperisTimesheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operis.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperisTimesheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
